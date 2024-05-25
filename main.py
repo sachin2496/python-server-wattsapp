7167f993ec1218d8cd80d8178e08747318567b12
sachin2496 <sachin.kumar.cse20@itbhu.ac.in>
2024-05-25 11:37:05 +0530

    first

---
+++ main.py
@@ -0,0 +1,23 @@
+﻿from flask import Flask, render_template, request
+from twilio.rest import Client
+
+app = Flask(__name__)
+
+
+
+@app.route('/send_whatsapp', methods=['POST'])
+def send_whatsapp():
+    print("hello world")
+    account_sid = 'AC36e5d266b52677fc40ac49767f7d08cb'
+    auth_token = 'f5c89c225cee95f7ca2d47171e2bb738'
+    from_whatsapp_number = 'whatsapp:+14155238886'
+    to_whatsapp_number = 'whatsapp:+917643927304'
+
+    client = Client(account_sid, auth_token)
+    message_body = request.form['message']
+    client.messages.create(body=message_body, from_=from_whatsapp_number, to=to_whatsapp_number)
+
+    return 'WhatsApp message sent successfully!'
+
+if __name__ == '__main__':
+    app.run(debug=True)
