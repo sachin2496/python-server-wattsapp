7b0e01afc8a536e50975d6543203dd6f32c486c3
sachin2496 <sachin.kumar.cse20@itbhu.ac.in>
2024-05-25 10:39:17 +0530

    first

--- sendEmail.py
+++ sendEmail.py
@@ -21,7 +21,7 @@ app = Flask(__name__)
 @app.route('/submit_form', methods=['POST'])
 def submit_form():
     account_sid = 'AC36e5d266b52677fc40ac49767f7d08cb'
-    auth_token = '1a123b9d22066b7af2eb1804822be71b'
+    auth_token = 'f5c89c225cee95f7ca2d47171e2bb738'
     client = Client(account_sid, auth_token)
 
     name = request.form['contactName']
