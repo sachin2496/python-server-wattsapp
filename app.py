f19c9164c0913354ab9d5305ea1f7cb47eceaeb1
sachin2496 <sachin.kumar.cse20@itbhu.ac.in>
2024-05-25 16:21:35 +0530

    last

--- app.py
+++ app.py
@@ -13,12 +13,18 @@ def send_whatsapp():
     auth_token = 'd33c2607996020d87e8f29820b793c0c'
     from_whatsapp_number = 'whatsapp:+14155238886'
     to_whatsapp_number = 'whatsapp:+918210072487'
-
+    name = request.form['contactName']
+    email = request.form['contactEmail']
+    message = request.form['message']
     client = Client(account_sid, auth_token)
-    message_body = request.form['message']
+    message_body = f'''Name:{name} \n
+                       Email:{email} \n
+                       Message:{message}
+                                    
+                                     '''
     client.messages.create(body=message_body, from_=from_whatsapp_number, to=to_whatsapp_number)
 
-    return 'WhatsApp message sent successfully!'
+    return 'message sent successfully!'
 
 if __name__ == '__main__':
     app.run(debug=True)
